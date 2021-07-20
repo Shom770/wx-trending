75300d8aa94a02115d9127295b7f1cdab56cbcb3
Shom770 <82843611+Shom770@users.noreply.github.com>
2021-07-20 16:09:09 -0400

    Allowing retweets

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ import tweepy
 
 def update_bot():
     try:
-        client = pymongo.MongoClient("mongodb+srv://Twitter:Sh070708@cluster0.nuqx6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+        client = pymongo.MongoClient(
+            "mongodb+srv://Twitter:Sh070708@cluster0.nuqx6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
         db = client['db']
         col = db['twitter']
         try:
@@ -19,7 +20,6 @@ def update_bot():
             file.close()
 
         words_to_remove = set(words_to_remove)
-
 
         auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
         auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
@@ -44,7 +44,7 @@ def update_bot():
 
         for tweet in bot.home_timeline(count=75):
             punctuation = "!()-[]{};:'\",<>./?@#$%^&*_~"
-            tweet_text = tweet.text.lower()
+            tweet_text = tweet.text.lower()[2:] if 'rt' in tweet_text else tweet.text.lower()
             if 'continues' in tweet_text or 'issues' in tweet_text or 'severe' in tweet_text or 'warning' in tweet_text or 'advisory' in tweet_text:
                 pass
             else:
@@ -54,14 +54,13 @@ def update_bot():
                 tweet_text = tweet_text.split(' ')
                 tweet_text = list(set(tweet_text) - words_to_remove)
 
-                if 'rt' not in tweet_text:
-                    if len(data[day].keys()) == 0:
-                        if tweet.created_at > datetime.datetime.fromisoformat(day):
-                            data[day][cur_time].append(tweet_text)
-                    else:
-                        if tweet.created_at > datetime.datetime.fromisoformat((keys_of_json := list(data[day].keys()))
-                                                                              [keys_of_json.index(cur_time) - 1]):
-                            data[day][cur_time].append(tweet_text)
+                if len(data[day].keys()) == 0:
+                    if tweet.created_at > datetime.datetime.fromisoformat(day):
+                        data[day][cur_time].append(tweet_text)
+                else:
+                    if tweet.created_at > datetime.datetime.fromisoformat((keys_of_json := list(data[day].keys()))
+                                                                          [keys_of_json.index(cur_time) - 1]):
+                        data[day][cur_time].append(tweet_text)
 
         if datetime.datetime.now().replace(hour=23, minute=43) < datetime.datetime.now() < \
                 datetime.datetime.now().replace(hour=23, minute=49):
