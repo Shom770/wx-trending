d47c5ae930e0f32325478d2ba6c1c27f1a86bce3
Shom770 <82843611+Shom770@users.noreply.github.com>
2021-07-20 14:24:24 -0400

    Replacing common words

--- main.py
+++ main.py
@@ -12,12 +12,16 @@ def update_bot():
         client = pymongo.MongoClient("mongodb+srv://Twitter:Sh070708@cluster0.nuqx6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
         db = client['db']
         col = db['twitter']
+        try:
+            file = open("words_to_remove.txt", "r")
+            words_to_remove = [line.strip() for line in file]
+        finally:
+            file.close()
+
 
         auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
         auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
         bot = tweepy.API(auth)
-
-        name = "WxTrending"
 
         day = datetime.datetime.today()
         day = f"{day.year}-{('0' if len(str(day.month)) == 1 else '') + str(day.month)}" \
@@ -38,17 +42,20 @@ def update_bot():
 
         for tweet in bot.home_timeline(count=75):
             punctuation = "!()-[]{};:'\",<>./?@#$%^&*_~"
-            tweet_text = tweet.text
-            for punc in punctuation:
-                tweet_text.replace(punc, '').replace('\u2026', '')
-            if tweet_text.split(' ')[0] != 'RT':
-                if len(data[day].keys()) == 0:
-                    if tweet.created_at > datetime.datetime.fromisoformat(day):
-                        data[day][cur_time].append(tweet_text.split(' '))
-                else:
-                    if tweet.created_at > datetime.datetime.fromisoformat((keys_of_json := list(data[day].keys()))
-                                                                          [keys_of_json.index(cur_time) - 1]):
-                        data[day][cur_time].append(tweet_text.split(' '))
+            tweet_text = tweet.text.lower()
+            if 'spc' in tweet_text or 'iembot' in tweet_text:
+                pass
+            else:
+                for punc, common_word in zip(punctuation, words_to_remove):
+                    tweet_text.replace(punc, '').replace(common_word, '')
+                if tweet_text.split(' ')[0] != 'RT':
+                    if len(data[day].keys()) == 0:
+                        if tweet.created_at > datetime.datetime.fromisoformat(day):
+                            data[day][cur_time].append(tweet_text.split(' '))
+                    else:
+                        if tweet.created_at > datetime.datetime.fromisoformat((keys_of_json := list(data[day].keys()))
+                                                                              [keys_of_json.index(cur_time) - 1]):
+                            data[day][cur_time].append(tweet_text.split(' '))
 
         if datetime.datetime.now().replace(hour=23, minute=43) < datetime.datetime.now() < \
                 datetime.datetime.now().replace(hour=23, minute=49):
