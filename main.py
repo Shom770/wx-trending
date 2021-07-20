836380846f5e047564e04264f648f09f94826f20
Shom770 <82843611+Shom770@users.noreply.github.com>
2021-07-20 14:02:52 -0400

    upping limit of count

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def update_bot():
 
         data[day][cur_time] = []
 
-        for tweet in bot.home_timeline(count=50):
+        for tweet in bot.home_timeline(count=75):
             punctuation = "!()-[]{};:'\",<>./?@#$%^&*_~"
             tweet_text = tweet.text
             for punc in punctuation:
